e82e5b534c66214b1afe5158b62751d2ec8e6d94
Fengyang Wang <fengyang.wang.0@gmail.com>
2015-08-19 23:25:44 -0400

    Work around several quirks in Findr API (hopefully will be addressed soon.)

--- src/main.jsx
+++ src/main.jsx
@@ -4,28 +4,56 @@ import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
 const MARKER_ICON = "https://cdnjs.cloudflare.com/ajax/libs/Leaflet.awesome-markers/2.0.2/images/markers-matte.png";
 
 const position = [0, 0];
-const map = (<Map center={position} zoom={5}>
-  <TileLayer
-    url="http://api.tiles.mapbox.com/v4/totalverb.30d348ce/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoidG90YWx2ZXJiIiwiYSI6ImE0NmY3YTE4MzhkZmM2MGU0MzY1MDgzZDE0ZGUyYzA4In0._CGLK1EG456RVASfRKtInQ"
-    attribution='<a href="https://www.mapbox.com/about/maps/" target="_blank">&copy; Mapbox &copy; OpenStreetMap</a> <a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a>'
-  />
-</Map>);
 
 class FindrClient extends React.Component {
   render() {
-    return map;
+    return <FindrMap />;
+  }
+}
+
+class FindrMap extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      events: []
+    };
+    this._findrMounted = true;
+  }
+
+  componentWillUnmount() {
+    this._findrMounted = false;
+  }
+
+  componentDidMount() {
+    // Fetch events and create markers for them.
+    fetch('http://eventfindr.herokuapp.com/events')
+      .then(response => response.json())
+      .then(events => {
+        if (this._findrMounted) {
+          this.setState({events: events.map(ev => {
+            ev = JSON.parse(ev);
+            ev.coordinates = ev.coordinates.map(Number);
+            return ev;
+          })});
+        }
+      });
+  }
+
+  render() {
+    return (<Map center={position} zoom={5}>
+      <TileLayer
+        url="http://api.tiles.mapbox.com/v4/totalverb.30d348ce/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoidG90YWx2ZXJiIiwiYSI6ImE0NmY3YTE4MzhkZmM2MGU0MzY1MDgzZDE0ZGUyYzA4In0._CGLK1EG456RVASfRKtInQ"
+        attribution='<a href="https://www.mapbox.com/about/maps/" target="_blank">&copy; Mapbox &copy; OpenStreetMap</a> <a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a>'
+      />
+      {
+        this.state.events.map(event => (<Marker position={event.coordinates} key={event.name}>
+          <Popup>
+            <span><b>{event.name}</b> by <b>{event['host-name']}</b></span>
+          </Popup>
+        </Marker>))
+      }
+    </Map>);
   }
 }
 
 React.render(<FindrClient />, document.body);
-
-function createMarkerForEvent(event) {
-  map.appendChild(<Marker icon={MARKER_ICON}>
-    <span><b>{event.name}</b> by <b>{event['host-name']}</b></span>
-  </Marker>);
-}
-
-// Fetch events and create markers for them.
-fetch('http://eventfindr.herokuapp.com/events')
-  .then(response => response.json())
-  .then(events => events.forEach(createMarkerForEvent));
