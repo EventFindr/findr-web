9d29ffc5566b087d5560a6dc85e131db71c7f840
Fengyang Wang <fengyang.wang.0@gmail.com>
2015-08-20 14:25:04 -0400

    Fix mixed content warnings on HTTPS. (Loads HTTPS always.)

--- src/main.jsx
+++ src/main.jsx
@@ -24,7 +24,7 @@ class FindrMap extends React.Component {
 
   componentDidMount() {
     // Fetch events and create markers for them.
-    fetch('http://eventfindr.herokuapp.com/events')
+    fetch('https://eventfindr.herokuapp.com/events')
       .then(response => response.json())
       .then(events => {
         if (this._findrMounted) {
@@ -40,7 +40,7 @@ class FindrMap extends React.Component {
   render() {
     return (<Map center={position} zoom={5}>
       <TileLayer
-        url="http://api.tiles.mapbox.com/v4/totalverb.30d348ce/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoidG90YWx2ZXJiIiwiYSI6ImE0NmY3YTE4MzhkZmM2MGU0MzY1MDgzZDE0ZGUyYzA4In0._CGLK1EG456RVASfRKtInQ"
+        url="https://api.tiles.mapbox.com/v4/totalverb.30d348ce/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoidG90YWx2ZXJiIiwiYSI6ImE0NmY3YTE4MzhkZmM2MGU0MzY1MDgzZDE0ZGUyYzA4In0._CGLK1EG456RVASfRKtInQ"
         attribution='<a href="https://www.mapbox.com/about/maps/" target="_blank">&copy; Mapbox &copy; OpenStreetMap</a> <a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a>'
       />
       {
